3742c3139875b1d6fb0e252bd5140e3e358ec6b9
Soedipo <dipo.lendou@gmail.com>
2025-03-11 20:48:39 +0700

    Add expense-tracker page

---
+++ src/pages/expense-tracker/index.jsx
@@ -0,0 +1,34 @@
+export const ExpenseTracker = () => {
+  return (
+    <>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1 className="text-lg font-bold">Expense Tracker</h1>
+          <div className="balance">
+            <h3>Your Balance</h3>
+            <h2>$191.00</h2>
+          </div>
+          <div className="summary">
+            <div className="income">
+              <h4>Income</h4>
+              <p>$200.00</p>
+            </div>
+          </div>
+          <form className="add-transaction">
+            <input type="text" placeholder="Description" required />
+            <input type="number" placeholder="Amount" required />
+            <input type="radio" id="expense" value="expense" />
+            <label htmlFor="expense">Expense</label>
+            <input type="radio" id="income" value="expense" />
+            <label htmlFor="income">Income</label>
+
+            <button type="submit">Add transaction</button>
+          </form>
+        </div>
+      </div>
+      <div className="transactions">
+        <h3>Transactions</h3>
+      </div>
+    </>
+  );
+};

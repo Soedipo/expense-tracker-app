81971e1e77bcd013a8ee004d7a50cd7be3dff211
Soedipo <dipo.lendou@gmail.com>
2025-03-12 00:32:21 +0700

    Add get transaction list function

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useAddTransaction } from "../../hooks/useAddTransaction";
+import { useGetTransactions } from "../../hooks/useGetTransactions";
 
 export const ExpenseTracker = () => {
   const { addTransaction } = useAddTransaction();
+  const { transactions } = useGetTransactions();
 
   const [description, setDescription] = useState("");
   const [transactionAmount, setTransactionAmount] = useState(0);
@@ -68,6 +70,20 @@ export const ExpenseTracker = () => {
       </div>
       <div className="transactions">
         <h3>Transactions</h3>
+        <ul>
+          {transactions.map((transaction) => {
+            const { description, transactionAmount, transactionType } =
+              transaction;
+            return (
+              <li>
+                <h4>{description}</h4>
+                <p>
+                  ${transactionAmount} • <label>{transactionType}</label>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );

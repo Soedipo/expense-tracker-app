1a61618cb191e9daf2ff143ba8199479c180d226
soedipo <soedipo@vyung.com>
2025-04-15 10:00:47 +0700

    add get category hook

--- src/hooks/useGetCategories.js
+++ src/hooks/useGetCategories.js
@@ -7,7 +7,6 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { db } from "../config/firebase-config";
-import { useGetUserInfo } from "./useGetUserInfo";
 
 export const useGetCategories = () => {
   const [categories, setCategories] = useState([]);

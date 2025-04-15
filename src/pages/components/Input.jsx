aadea8508b8937d188758e11cf3aa27c1f12f115
soedipo <soedipo@vyung.com>
2025-04-15 12:40:29 +0700

    Add Input component

---
+++ src/pages/components/Input.jsx
@@ -0,0 +1,14 @@
+// src/components/Input.jsx
+export const Input = ({ type = "text", placeholder, value, onChange, ...props }) => {
+    return (
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="border border-zinc-700 rounded w-full bg-zinc-700 text-white placeholder-gray-400 focus:outline-none focus:ring-0 focus:border-zinc-500"
+        {...props}
+      />
+    );
+  };
+  

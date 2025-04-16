238a4d17225699634361d875f58e0314ce5547f3
soedipo <soedipo@vyung.com>
2025-04-16 08:44:01 +0700

    add Id as param in Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,14 @@
 // src/components/Input.jsx
-export const Input = ({ type = "text", placeholder, value, onChange, ...props }) => {
-    return (
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className="border border-zinc-700 rounded w-full px-1 -ml-1 bg-zinc-700 text-white placeholder-gray-400 focus:outline-none focus:ring-0 focus:border-zinc-500"
-        {...props}
-      />
-    );
-  };
-  
+export const Input = ({ id, type = "text", placeholder, value, onChange, ...props }) => {
+  return (
+    <input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="border border-zinc-700 rounded w-full px-1 -ml-1 bg-zinc-700 text-white placeholder-gray-400 focus:outline-none focus:ring-0 focus:border-zinc-500"
+      {...props}
+    />
+  );
+};

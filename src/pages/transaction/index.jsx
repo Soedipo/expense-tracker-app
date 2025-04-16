0a05ae600dfd1b9fc857f3644955747a54c7d1b3
soedipo <soedipo@vyung.com>
2025-04-16 15:05:34 +0700

    change name

--- src/pages/transaction/index.jsx
+++ src/pages/transaction/index.jsx
@@ -116,7 +116,7 @@ export const Transaction = () => {
         <SideNav />
         {/* Main Content */}
         <div className="expense-tracker ml-64 w-11/12 flex flex-col items-center gap-6 p-4 bg-zinc-700 min-h-screen text-white">
-          <h1 className="text-3xl font-bold text-center mb-6 mt-6">Expense Tracker</h1>
+          <h1 className="text-3xl font-bold text-center mb-6 mt-6">Transactions</h1>
 
           {/* Balance & Summary */}
           <div className="info-container flex w-full max-w-3xl">
